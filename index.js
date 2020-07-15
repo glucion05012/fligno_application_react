72c15b86890770dbe6495e86d81cd073fabf2163
Ágata Jiménez <giddel.lucion@yahoo.com>
2020-07-15 19:38:00 +0800

    dbConnection to heroku

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log('listening to port ' + PORT)
-})
-
-app.use(express.static('src/index.js'))
